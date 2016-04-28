27c4808f39da6953391aa2ae91a6a0eca308f5bc
Tim Walter <tim@twwd.de>
2016-04-28 21:53:32 +0200

    switched from sqlite to yaml

--- downloader.py
+++ downloader.py
@@ -1,46 +1,46 @@
 #!/usr/bin/env python3
 import os
 import re
-import sqlite3
 from urllib.parse import urljoin
 
 import requests
+import yaml
 from bs4 import BeautifulSoup
 
-# Connect to database
-conn = sqlite3.connect(os.path.join('data', 'moodle_downloader.db'))
-c = conn.cursor()
-c2 = conn.cursor()
+# import config
+with open(os.path.join('data', 'config.yaml'), 'r') as config_file:
+    config = yaml.load(config_file)
+    print(config)
+    # disable HTTPS warnings
+    requests.packages.urllib3.disable_warnings()
+    # make the initial request to get the token
+    session = requests.Session()
 
-# disable HTTPS warnings
-requests.packages.urllib3.disable_warnings()
-# make the initial request to get the token
-session = requests.Session()
+    # Loop through moodles
+    for moodle in config:
+        print('\nCurrent moodle is %s' % moodle['name'])
+        # login
+        response = session.get(moodle['login_url'])
+        # TODO
+        if response.url != '' and response.text.find('Anmeldung erfolgreich') == -1:
+            print(response.url)
+            # borrowed from Dominik
+            match = re.search(r'<input type="hidden" name="lt" value="(.*?)" />', response.text)
+            token = match.group(1)
+            match = re.search(r'name="execution" value="(.*?)"', response.text)
+            execution = match.group(1)
+            # do the real login
+            params = {"username": moodle['username'], "password": moodle['password'], "lt": token,
+                      "execution": execution, "_eventId": "submit",
+                      "submit": "ANMELDEN"}
+            session.post(response.url, params)
 
-# Loop through moodles
-for moodle in c2.execute('SELECT * FROM moodle'):
-    print('Current moodle is %s' % moodle[1])
-    # login
-    response = session.get(moodle[2])
-    if response.url != '' and response.text.find('Anmeldung erfolgreich') == -1:
-        print('do login')
-        print(response.url)
-        # borrowed from Dominik
-        match = re.search(r'<input type="hidden" name="lt" value="(.*?)" />', response.text)
-        token = match.group(1)
-        match = re.search(r'name="execution" value="(.*?)"', response.text)
-        execution = match.group(1)
-        # do the real login
-        params = {"username": moodle[3], "password": moodle[4], "lt": token,
-                  "execution": execution, "_eventId": "submit",
-                  "submit": "ANMELDEN"}
-        session.post(response.url, params)
-
-    for course in c.execute('SELECT * FROM course WHERE moodle=?', (moodle[0],)):
-        print(course[5])
-        course_url = urljoin(moodle[1], '/course/view.php?id=' + str(course[1]))
-        course_content = BeautifulSoup(session.get(course_url).text, 'html.parser').find(id='region-main')
-        for link_text in course_content.find_all(string=re.compile(course[3])):
-            link = link_text.find_parent('a')
-            if link is not None:
-                print(link['href'])
+        # loop through courses
+        for course in moodle['courses']:
+            print(course['name'])
+            course_url = urljoin(moodle['base_url'], '/course/view.php?id=' + str(course['id']))
+            course_content = BeautifulSoup(session.get(course_url).text, 'html.parser').find(id='region-main')
+            for link_text in course_content.find_all(string=re.compile(course['pattern'])):
+                link = link_text.find_parent('a')
+                if link is not None:
+                    print(link['href'])
