0981ecd02ceab397fbbddc63e6043c822bb05238
Tim Walter <tim@twwd.de>
2016-04-28 19:40:35 +0200

    another commit

--- downloader.py
+++ downloader.py
@@ -5,10 +5,12 @@ import sqlite3
 from urllib.parse import urljoin
 
 import requests
-
 # Connect to database
+from bs4 import BeautifulSoup
+
 conn = sqlite3.connect(os.path.join('data', 'moodle_downloader.db'))
 c = conn.cursor()
+c2 = conn.cursor()
 
 # disable HTTPS warnings
 requests.packages.urllib3.disable_warnings()
@@ -16,10 +18,14 @@ requests.packages.urllib3.disable_warnings()
 session = requests.Session()
 
 # Loop through moodles
-for moodle in c.execute('SELECT * FROM moodle'):
+for moodle in c2.execute('SELECT * FROM moodle'):
+    print('Current moodle is %s' % moodle[1])
     # login
-    response = session.get(moodle[2], allow_redirects=True)
-    if response.text.find('Anmeldung erfolgreich') == -1:
+    response = session.get(moodle[2])
+    if response.url != '' and response.text.find('Anmeldung erfolgreich') == -1:
+        print('do login')
+        print(response.url)
+        # lent from Dominik
         match = re.search(r'<input type="hidden" name="lt" value="(.*?)" />', response.text)
         token = match.group(1)
         match = re.search(r'name="execution" value="(.*?)"', response.text)
@@ -32,8 +38,10 @@ for moodle in c.execute('SELECT * FROM moodle'):
         response = session.get(moodle[1])
 
     for course in c.execute('SELECT * FROM course WHERE moodle=?', (moodle[0],)):
+        print(course[5])
         course_url = urljoin(moodle[1], '/course/view.php?id=' + str(course[1]))
-        print(course_url)
-        course_content = session.get(course_url).text
-        for file in re.findall(r'Vorlesung.*', course_content):  # '(%s)' % course[2], course_content):
-            print(file)
+        course_content = BeautifulSoup(session.get(course_url).text, 'html.parser').find(id='region-main')
+        for link_text in course_content.find_all(string=re.compile(course[3])):
+            link = link_text.find_parent('a')
+            if link is not None:
+                print(link['href'])
