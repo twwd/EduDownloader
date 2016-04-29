39fcdd51e8f2c2a39022e1988e63e0253734cbfd
Tim Walter <tim@twwd.de>
2016-04-29 08:44:50 +0200

    add downloading files

--- downloader.py
+++ downloader.py
@@ -7,6 +7,8 @@ import requests
 import yaml
 from bs4 import BeautifulSoup
 
+debug = True
+
 # import config
 with open(os.path.join('data', 'config.yaml'), 'r') as config_file:
     config = yaml.load(config_file)
@@ -17,14 +19,14 @@ with open(os.path.join('data', 'config.yaml'), 'r') as config_file:
 
     # Loop through moodles
     for moodle in config:
-        print('\nCurrent moodle is %s' % moodle['name'])
+        if debug:
+            print('\nCurrent moodle is %s' % moodle['name'])
 
         # login
-        response = session.get(urljoin(moodle['base_url'], 'my'))
+        response = session.get(urljoin(moodle['base_url'], 'my'), allow_redirects=False)
         # check if the user is already signed in
         # TODO
-        if response.status_code != -1:
-            print(response.status_code)
+        if response.status_code != 301:
             response = session.get(moodle['login_url'])
             # borrowed from Dominik
             match = re.search(r'<input type="hidden" name="lt" value="(.*?)" />', response.text)
@@ -39,10 +41,19 @@ with open(os.path.join('data', 'config.yaml'), 'r') as config_file:
 
         # loop through courses
         for course in moodle['courses']:
-            print(course['name'])
+            if debug:
+                print(course['name'])
             course_url = urljoin(moodle['base_url'], '/course/view.php?id=' + str(course['id']))
             course_content = BeautifulSoup(session.get(course_url).text, 'html.parser').find(id='region-main')
             for link_text in course_content.find_all(string=re.compile(course['pattern'])):
                 link = link_text.find_parent('a')
                 if link is not None:
-                    print(link['href'])
+                    file_request = session.get(link['href'])
+                    file_disposition = file_request.headers['Content-Disposition']
+                    file_name = file_disposition[
+                                file_disposition.index('filename=') + 10:len(file_disposition) - 1].encode(
+                        "latin-1").decode("utf8")
+                    if debug:
+                        print(file_name)
+                    with open(os.path.join(course['local_folder'], file_name), 'wb') as f:
+                        f.write(file_request.content)
