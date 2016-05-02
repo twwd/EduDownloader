8e877b511427743236e694181aca80f9b958f596
Tim Walter <tim@twwd.de>
2016-05-02 14:29:20 +0200

    fix for files with duplicate name

--- downloader.py
+++ downloader.py
@@ -73,33 +73,49 @@ with open(os.path.join('data', 'config.yaml'), 'r') as config_file:
 
             for link_text in course_content.find_all(string=re.compile(course['pattern'])):
                 link = link_text.find_parent('a')
-                if link is not None:
-                    # request file
-                    file_request = session.get(link['href'])
+                if link is not None and link['href'].find('resource') != -1:
+                    # request file http header
+                    file_request = session.head(link['href'], allow_redirects=True)
+
                     # get file name
-                    file_disposition = file_request.headers['Content-Disposition']
-                    file_name = file_disposition[
-                                file_disposition.index('filename=') + 10:len(file_disposition) - 1].encode(
-                        "latin-1").decode("utf8")
+                    if 'Content-Disposition' in file_request.headers:
+                        file_disposition = file_request.headers['Content-Disposition']
+                        file_name = file_disposition[
+                                    file_disposition.index('filename=') + 10:len(file_disposition) - 1].encode(
+                            "latin-1").decode("utf8")
+                    else:
+                        file_name = link_text
+
                     # get last modified date as timestamp
                     file_last_modified = int(datetime.strptime(file_request.headers['Last-Modified'],
                                                                '%a, %d %b %Y %H:%M:%S %Z').timestamp())
+                    # adjust file name
+                    if 'rename' in course and course['rename'] is not False:
+                        # find a number
+                        num = re.search('\d{1,2}', link_text)
+                        if num is None:
+                            num = re.search('\d{1,2}', file_name)
+                        if num is None:
+                            num = file_last_modified
+                        else:
+                            num = num.group(0)
+                        file_name = course['rename'].replace('%', str(num))
+
                     if verbose_output:
                         print(file_name)
-                        print(int(file_last_modified))
 
                     # fetch old timestamp from database
                     file_last_modified_old = c.execute(
                         'SELECT last_modified FROM timestamp WHERE course=? AND file_name=?',
-                        (course['name'], file_name)).fetchone()[0]
+                        (course['name'], file_name)).fetchone()
 
                     # save file and timestamp in the database if it doesn't exists
                     if file_last_modified_old is None:
                         c.execute('INSERT INTO timestamp (course, file_name, last_modified) VALUES (?,?,?)',
                                   (course['name'], file_name, file_last_modified))
                     # update timestamp if there's a newer version of the file
-                    elif file_last_modified > file_last_modified_old:
-                        c.execute('UPDATE (last_modified) VALUES (?) FROM timestamp WHERE course=? and file_name=?',
+                    elif file_last_modified > file_last_modified_old[0]:
+                        c.execute('UPDATE timestamp SET last_modified=? WHERE course=? and file_name=?',
                                   (file_last_modified, course['name'], file_name))
                     # otherwise skip saving
                     else:
@@ -108,6 +124,9 @@ with open(os.path.join('data', 'config.yaml'), 'r') as config_file:
 
                     # save changes to the database
                     conn.commit()
+
+                    # request whole file
+                    file_request = session.get(link['href'])
 
                     # write file
                     with open(os.path.join(course['local_folder'], file_name), 'wb') as f:
